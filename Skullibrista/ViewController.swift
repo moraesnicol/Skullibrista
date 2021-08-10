c9fc45f01436c0b5ed9e9aa75b8b5e4dacc0cf6b
Gabriel <marcos.santos@e-deploy.com.br>
2021-08-10 00:51:13 -0300

    Initial Commit

---
+++ Skullibrista/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skullibrista
+//
+//  Created by Gabriel on 10/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
